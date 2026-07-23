278cd706d2163c6dbcba9875cff505aa7953ac71
Zero <filip.smitak@gmail.com>
2026-07-23 14:54:33 +0200

    Added styles file

---
+++ Gogame/Views/GameResultDialog.axaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+
+namespace Gogame.Views
+{
+    public partial class GameResultDialog : UserControl
+    {
+        public GameResultDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
